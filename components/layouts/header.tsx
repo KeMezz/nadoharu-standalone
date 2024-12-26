09b67f3cf74ada0f8818a0167862a5e72cf8619a
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-12-26 15:57:34 +0900

    뒤로 가기 버튼 표시를 옵션 추가

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 
 interface HeaderProps {
   title?: string;
+  canGoBack?: boolean;
   profile?: {
     id: number;
     username: string;
@@ -16,20 +17,26 @@ interface HeaderProps {
   children?: React.ReactNode;
 }
 
-export default function Header({ title, profile, children }: HeaderProps) {
+export default function Header({
+  title,
+  canGoBack,
+  profile,
+  children,
+}: HeaderProps) {
   const router = useRouter();
   return (
     <header className="px-4 h-14 border-b-2 border-violet-600 dark:border-violet-400 text-center fixed flex justify-between top-0 max-w-2xl w-full bg-white dark:bg-neutral-800 z-20 shadow-md">
       <section className="flex items-center gap-4">
+        {canGoBack ? (
+          <button>
+            <ChevronLeftIcon onClick={router.back} className="size-6" />
+          </button>
+        ) : null}
         {title ? (
           <h1 className="text-lg font-bold text-violet-600 dark:text-violet-400">
             {title}
           </h1>
-        ) : (
-          <button>
-            <ChevronLeftIcon onClick={router.back} className="size-6" />
-          </button>
-        )}
+        ) : null}
         {profile ? (
           <Link
             href={`/users/${profile.login_id}`}
