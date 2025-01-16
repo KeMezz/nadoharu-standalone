c8d287638afc3d9922524ccf79092f34af8fdd80
정형진 <88423849+KeMezz@users.noreply.github.com>
2025-01-17 00:06:18 +0900

    프로필 수정 페이지 디자인 작성

---
+++ components/forms/edit-profile-form.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { PencilIcon } from "@heroicons/react/24/solid";
+import ProfileImage from "../profile-image";
+import { useState } from "react";
+import Textarea from "../inputs/textarea";
+import { useForm } from "react-hook-form";
+
+interface EditProfileFormProps {
+  username: string;
+  avatar: string | null;
+  login_id: string;
+  bio: string | null;
+}
+
+interface EditProfileFormData {
+  bio: string;
+  avatar: File | null;
+}
+
+export default function EditProfileForm({
+  username,
+  avatar,
+  login_id,
+  bio,
+}: EditProfileFormProps) {
+  const { register, handleSubmit } = useForm<EditProfileFormData>({
+    defaultValues: { bio: bio ?? "" },
+  });
+
+  const [preview, setPreview] = useState<string | null>(avatar);
+  const onImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setPreview(URL.createObjectURL(file));
+    }
+  };
+
+  const onSubmit = (data: EditProfileFormData) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="bg-neutral-100 dark:bg-neutral-800 flex flex-col justify-end p-4 gap-3 relative">
+        <div className="flex flex-col gap-3">
+          <div className="size-20 rounded-md relative overflow-hidden">
+            <label
+              htmlFor="avatar"
+              className="flex items-center justify-center absolute top-0 left-0 size-20 opacity-40 bg-neutral-200 dark:bg-neutral-600 hover:opacity-100 cursor-pointer"
+            >
+              <PencilIcon className="size-6" />
+              <input
+                type="file"
+                className="hidden"
+                id="avatar"
+                {...register("avatar")}
+                onChange={onImageChange}
+              />
+            </label>
+            <ProfileImage avatar={preview} username={username} size={20} />
+          </div>
+          <div className="flex flex-col">
+            <div className="flex items-center gap-3">
+              <h5 className="font-semibold">{username}</h5>
+            </div>
+            <p className="text-gray-400 text-sm">@{login_id}</p>
+          </div>
+        </div>
+        <Textarea placeholder="자기소개 작성.." {...register("bio")} />
+        <button className="mt-4 bg-violet-400 dark:bg-violet-600 focus:ring-violet-600 text-white w-full py-2 rounded-md outline-none focus:ring-2">
+          프로필 업데이트
+        </button>
+      </div>
+    </form>
+  );
+}
