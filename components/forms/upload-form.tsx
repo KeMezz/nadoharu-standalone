2e8f72fe6cf8763a4d490a631675b7ecaf53995e
정형진 <88423849+KeMezz@users.noreply.github.com>
2025-01-20 22:47:29 +0900

    이미지 4개 업로드 외관 작성

--- components/forms/upload-form.tsx
+++ components/forms/upload-form.tsx
@@ -4,11 +4,40 @@ import { PlusIcon } from "@heroicons/react/24/solid";
 import SubmitButton from "../buttons/submit-button";
 import TextInput from "../inputs/text-input";
 import Textarea from "../inputs/textarea";
-import { useActionState } from "react";
+import { useActionState, useState } from "react";
 import { uploadPost } from "@/app/posts/upload/action";
+import { useSetAtom } from "jotai";
+import { alertAtom } from "@/libs/atoms";
+import Image from "next/image";
+
+const maxImages = 4;
 
 export default function UploadForm() {
   const [state, action] = useActionState(uploadPost, null);
+  const [previews, setPreviews] = useState<string[]>([]);
+  const setAlert = useSetAtom(alertAtom);
+
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!files) return;
+
+    // 이미 업로드된 이미지 수 확인
+    if (previews.length + files.length > maxImages) {
+      setAlert({
+        visible: true,
+        title: "너무 많아요!",
+        description: `이미지는 최대 ${maxImages}개까지만 업로드할 수 있어요.`,
+      });
+      return;
+    }
+
+    // 파일 미리보기 생성
+    const newPreviews = Array.from(files).map((file) =>
+      URL.createObjectURL(file)
+    );
+    setPreviews((prev) => [...prev, ...newPreviews]);
+  };
+
   return (
     <form action={action} className="flex flex-col gap-3">
       <Textarea
@@ -22,14 +51,44 @@ export default function UploadForm() {
         placeholder="태그 작성.."
         errors={state?.fieldErrors.tags}
       />
-      <div>
-        <label
-          htmlFor="media"
-          className="w-14 h-14 border-2 border-dashed rounded-md text-neutral-300 flex justify-center items-center hover:text-neutral-700 hover:border-neutral-700 dark:hover:text-neutral-400 dark:hover:border-neutral-400 hover:cursor-pointer"
-        >
-          <PlusIcon className="size-6" />
-        </label>
-        <input type="file" className="hidden" id="media" />
+      <div className="flex gap-2">
+        <div>
+          <label
+            htmlFor="photos"
+            className={`w-14 h-14 border-2 border-dashed rounded-md text-neutral-300 flex justify-center items-center ${
+              previews.length >= maxImages
+                ? "opacity-50 cursor-not-allowed"
+                : "cursor-pointer hover:text-neutral-700 hover:border-neutral-700 dark:hover:text-neutral-400 dark:hover:border-neutral-400"
+            }`}
+          >
+            <PlusIcon
+              className={`size-6 ${
+                previews.length >= maxImages ? "rotate-45" : ""
+              }`}
+            />
+          </label>
+          <input
+            multiple
+            type="file"
+            accept="image/*"
+            className="hidden"
+            id="photos"
+            name="photos"
+            disabled={previews.length >= maxImages}
+            onChange={onFileChange}
+          />
+        </div>
+        {previews.map((preview, index) => (
+          <button key={index} type="button">
+            <Image
+              src={preview}
+              alt="preview"
+              className="size-14 rounded-md"
+              width={56}
+              height={56}
+            />
+          </button>
+        ))}
       </div>
       <SubmitButton text="글쓰기" />
     </form>
