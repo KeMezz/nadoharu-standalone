b6c4f7232072ab1e21467e41653606116eea2c23
정형진 <88423849+KeMezz@users.noreply.github.com>
2025-01-29 01:11:08 +0900

    로딩 디자인 및 파일 디렉토리 수정

--- components/inputs/chat-input.tsx
+++ components/inputs/chat-input.tsx
@@ -3,9 +3,11 @@ import { InputHTMLAttributes } from "react";
 
 interface ChatInputProps {
   errors?: string[];
+  pending?: boolean;
 }
 
 export default function ChatInput({
+  pending,
   ...attrs
 }: ChatInputProps & InputHTMLAttributes<HTMLInputElement>) {
   return (
@@ -13,13 +15,17 @@ export default function ChatInput({
       <div className="relative">
         <input
           type="text"
-          className="w-full shadow-md pl-4 pr-14 py-3 rounded-3xl focus:ring-2 ring-violet-400 focus:outline-none dark:bg-neutral-600 dark:text-white"
+          className="w-full ring-2 ring-neutral-200 dark:ring-neutral-600 pl-4 pr-14 py-3 rounded-3xl focus:ring-2 focus:ring-violet-400 focus:outline-none dark:bg-neutral-600 dark:text-white disabled:bg-neutral-200 disabled:text-neutral-400 disabled:cursor-not-allowed"
           autoCapitalize="none"
           autoCorrect="off"
           autoSave="off"
+          disabled={pending}
           {...attrs}
         />
-        <button className="absolute right-2 top-[7px] text-md p-2 bg-violet-400 rounded-3xl text-white">
+        <button
+          className="absolute right-2 top-[7px] text-md p-2 bg-violet-400 rounded-3xl text-white disabled:bg-neutral-400 disabled:text-neutral-800 disabled:cursor-not-allowed"
+          disabled={pending}
+        >
           <ArrowUpIcon className="size-5" />
         </button>
       </div>
