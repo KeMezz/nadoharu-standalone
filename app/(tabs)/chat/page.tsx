913a90a648739d1942ab15be6c23da9369035372
정형진 <88423849+KeMezz@users.noreply.github.com>
2025-04-01 15:25:52 +0900

    채팅 리스트 화면 작성

--- app/(tabs)/chat/page.tsx
+++ app/(tabs)/chat/page.tsx
@@ -1,22 +1,85 @@
 import ProfileImage from "@/components/domains/profile/image";
 import Link from "next/link";
+import getSession from "@/libs/session";
+import db from "@/libs/db";
+import EmptyState from "@/components/layouts/empty-state";
 
-export default function Chats() {
+async function getChatRooms() {
+  const session = await getSession();
+  const chatRooms = await db.chatRoom.findMany({
+    where: {
+      OR: [{ initiatorId: session?.id }, { recipientId: session?.id }],
+    },
+    include: {
+      chatMessages: {
+        select: {
+          content: true,
+          createdAt: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: 1,
+      },
+      initiator: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+      recipient: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+
+  return chatRooms;
+}
+
+export default async function Chats() {
+  const session = await getSession();
+  const chatRooms = await getChatRooms();
+
   return (
-    <section className="flex flex-col divide-y divide-neutral-500">
-      {Array.from({ length: 10 }).map((_, index) => (
-        <Link
-          href={`/chat/${index}`}
-          key={index}
-          className="flex items-center gap-3 p-4 hover:bg-neutral-100 dark:hover:bg-neutral-800"
-        >
-          <ProfileImage avatar={null} username="test" size={10} />
-          <div className="flex flex-col">
-            <p className="text-sm font-bold">John Doe</p>
-            <p className="text-sm text-neutral-500">Last message</p>
-          </div>
-        </Link>
-      ))}
+    <section className="flex flex-col divide-y divide-neutral-500 py-1">
+      {chatRooms.length === 0 ? (
+        <EmptyState text="아직 채팅방이 없어요" />
+      ) : (
+        chatRooms.map((chatRoom) => (
+          <Link
+            key={chatRoom.id}
+            href={`/chat/${chatRoom.id}`}
+            className="flex items-center gap-4 p-4 hover:bg-neutral-100 dark:hover:bg-neutral-800"
+          >
+            <ProfileImage
+              avatar={
+                chatRoom.initiatorId === session?.id
+                  ? chatRoom.recipient.avatar
+                  : chatRoom.initiator.avatar
+              }
+              username={
+                chatRoom.initiatorId === session?.id
+                  ? chatRoom.recipient.username
+                  : chatRoom.initiator.username
+              }
+              size={10}
+            />
+            <div className="flex flex-col">
+              <p className="font-bold">
+                {chatRoom.initiatorId === session?.id
+                  ? chatRoom.recipient.username
+                  : chatRoom.initiator.username}
+              </p>
+              <p className="font-medium text-neutral-500">
+                {chatRoom.chatMessages[0].content}
+              </p>
+            </div>
+          </Link>
+        ))
+      )}
     </section>
   );
 }
